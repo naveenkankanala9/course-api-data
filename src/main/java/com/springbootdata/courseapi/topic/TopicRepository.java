e0d8d0c5271e8a291145407493b0dd0dfa060326
Naveen Kumar Reddy Kankanala <naveenkankanala9@gmail.com>
2018-01-03 14:16:14 -0500

    spring Boot data

---
+++ src/main/java/com/springbootdata/courseapi/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.springbootdata.courseapi.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
